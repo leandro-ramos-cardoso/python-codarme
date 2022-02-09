c24b43b7af1ec4fa8691976fffa0dcb5401c0b98
leandro-ramos-cardoso <97923423+leandro-ramos-cardoso@users.noreply.github.com>
2022-02-09 09:51:54 -0300

    Update ex001.py

--- modulo-02/ex001.py
+++ modulo-02/ex001.py
@@ -1,9 +1,18 @@
+# Escreva um programa que contenha 4 variáveis e que imprima na tela o tipo de cada uma delas. A saída do seu programa deve ser na seguinte ordem:
+
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'bool'>
+
+print('============{:^20}============'.format('EXERCICIO 01'))
+
 nome = 'Manuela'
 x = 28
-z = 10/5
+z = 10.5
 a = 2 == 2
 
-print(type(nome))
-print(type(x))
-print(type(z))
-print(type(a))
+print('Tipo da váriavel {} -> {}'.format(nome, type(nome)))
+print('Tipo da váriavel {} -> {}'.format(x, type(x)))
+print('Tipo da váriavel {} -> {}'.format(z, type(z)))
+print('Tipo da váriavel {} -> {}'.format(a, type(a)))
