2ef73ab1023b8832e50c37bd205c3bfac9862cfe
leandro-ramos-cardoso <97923423+leandro-ramos-cardoso@users.noreply.github.com>
2022-02-09 09:33:53 -0300

    Add files via upload

---
+++ modulo-02/ex001.py
@@ -0,0 +1,9 @@
+nome = 'Manuela'
+x = 28
+z = 10/5
+a = 2 == 2
+
+print(type(nome))
+print(type(x))
+print(type(z))
+print(type(a))
---
+++ modulo-02/ex002.py
@@ -0,0 +1,8 @@
+valor_compras = 250.70
+desconto = 50.70
+quantidade_itens = 10
+valor_final = valor_compras - desconto
+
+print('========={:^10}========='.format('EX 002'))
+print('Valor final das compras -> R$ {}'.format(valor_final))
+print('Custo médido -> R$ {}'.format(valor_final / 10))
