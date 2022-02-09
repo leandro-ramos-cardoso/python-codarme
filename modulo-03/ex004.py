6c05123cc0eccc40ee092adbd8838a7e513ea707
leandro-ramos-cardoso <97923423+leandro-ramos-cardoso@users.noreply.github.com>
2022-02-09 13:54:01 -0300

    Update ex004.py

--- modulo-03/ex004.py
+++ modulo-03/ex004.py
@@ -1,4 +1,5 @@
-# Alice é responsável por escrever um programa que verifica se um cupom de desconto pode ser utilizado. O programa recebe 3 valores e retorna True caso o cupom possa ser utilizado, ou False , caso contrário.
+# Alice é responsável por escrever um programa que verifica se um cupom de desconto pode ser utilizado. O programa recebe 3 valores e retorna True 
+# caso o cupom possa ser utilizado, ou False , caso contrário.
 
 # Os três valores que o programa recebe são:
 # 1. Valor de compra (float)
@@ -7,7 +8,8 @@
 
 # O cupom tem a seguinte regra:
 
-# Caso o valor da compra somado ao frete seja superior a R$ 100, ou o cliente seja cadastrado no programa de fidelidade, o cupom de desconto pode ser utilizado. Caso contrário, o cupom não pode ser utilizado. Seu objetivo é implementar o programa que atenda a especificação acima.
+# Caso o valor da compra somado ao frete seja superior a R$ 100, ou o cliente seja cadastrado no programa de fidelidade, o cupom de desconto pode ser utilizado. 
+# Caso contrário, o cupom não pode ser utilizado. Seu objetivo é implementar o programa que atenda a especificação acima.
 
 valor_compra = float(input('Digite o valor de compra: '))
 valor_frete = float(input('Digite o valor do frete: '))
