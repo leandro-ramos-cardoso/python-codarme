94b2e7c2155d10f7424969f45be0b36d387f85b2
leandro-ramos-cardoso <97923423+leandro-ramos-cardoso@users.noreply.github.com>
2022-02-09 11:16:34 -0300

    Add files via upload

---
+++ modulo-03/aula02.py
@@ -0,0 +1,10 @@
+# OPERADORES RELACIONAIS (TRUE OR FALSE)
+
+x = int(input('Digite um número: '))
+z = int(input('Digite outro número: '))
+
+print(x > z)
+print(x < z)
+print(x == z)
+print(x >= z)
+print(x <= z)
